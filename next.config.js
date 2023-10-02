6124564e04dcc53ad624d5e5d24e2f757adde378
cassiusdc <cassiusdelacruz@gmail.com>
2023-10-02 15:14:50 +0800

    styled branches list

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['inline.imgix.net'],
+    domains: ['inline.imgix.net', 'upload.wikimedia.org'],
   },
 }
 
--- src/app/components/restaurant/branches-list.tsx
+++ src/app/components/restaurant/branches-list.tsx
@@ -1,30 +1,57 @@
 "use client"
 
-import { Branch, Restaurant } from "@prisma/client";
+import { Branch } from "@prisma/client";
 import Image from "next/image";
 import { ScrollArea }  from "@radix-ui/react-scroll-area"
-import { usePathname, useRouter } from "next/navigation";
+import * as Separator from '@radix-ui/react-separator';
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 interface BranchesListProps{
   branches: Branch[]
 }
 
-export const BranchesList = async({
+export const BranchesList = ({
   branches
 }: BranchesListProps) => {
   const asPath = usePathname();
-  console.log(asPath)
+
   return (  
     <section className="flex flex-col gap-y-4">
               <p className="text-xl md:text-3xl font-medium">Branches</p>
-              <ScrollArea className="flex-1 w-full">
-              {branches.map((branch)=> (
-                    <div key={branch.id} className="mb-4">
-                        <div>
-                          <a href={asPath + `/`+ branch.id + `/reservation`}>{branch.name} Branch </a>
-                        </div>
-                    </div>
-                ))}
-              </ScrollArea>
+              <div>
+                <ScrollArea className="flex-1 w-full">
+                  <ul>
+                    {branches.map((branch, index)=> (
+                        <li key={branch.id}>
+                          <Link key={branch.id} href={`${asPath}/${branch.id}/reservation`}>
+                            <div className="flex py-4 hover:bg-zinc-300 transition items-center">
+                              <Image
+                                src={branch.imageUrl}
+                                alt={branch.name}
+                                width={100}
+                                height={100}
+                                className="rounded" />
+                              <div className="ml-4">
+                                <p className="text-sm md:text-base font-medium">
+                                  {branch.name}
+                                </p>
+                                <p className="text-xs md:text-base font-light">
+                                  {branch.address}
+                                </p>
+                              </div>
+                            </div>
+                          </Link>
+                          {!(index + 1 === branches.length) && (
+                            <Separator.Root
+                              orientation="horizontal"
+                              className="bg-zinc-600/30 h-[1px] w-full" 
+                            />
+                          )}
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
       </section>
   );
 }
