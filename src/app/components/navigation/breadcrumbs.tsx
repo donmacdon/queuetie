b46f3b7cf99f54a989cf4c231b61e5b657a57501
cassiusdc <cassiusdelacruz@gmail.com>
2023-10-02 21:49:47 +0800

    navbar behavior and breadcrumbs styling

--- src/app/components/navigation/breadcrumbs.tsx
+++ src/app/components/navigation/breadcrumbs.tsx
@@ -1,13 +1,22 @@
 "use client"
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
 
 export const Breadcrumbs = () => {
   
 
   return (  
     <NavigationMenu.List className="flex gap-2 items-center">
-      <NavigationMenu.Item>Test</NavigationMenu.Item>
-      <NavigationMenu.Item>Test 2</NavigationMenu.Item>
+      <NavigationMenu.Item>
+        <Link href="#" className="hover:link-hover">Breadcrumb-1</Link>
+      </NavigationMenu.Item>
+      <ChevronRight className="h-3 w-3" />
+      <NavigationMenu.Item>
+        <Link href="#" className="hover:link-hover">
+          Breadcrumb-2
+        </Link>
+      </NavigationMenu.Item>
     </NavigationMenu.List>  
   );
 }
--- src/app/components/navigation/navigation-bar.tsx
+++ src/app/components/navigation/navigation-bar.tsx
@@ -1,14 +1,34 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import { Breadcrumbs } from "./breadcrumbs";
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 
 export const NavigationBar = () => {
+  const [visible, setVisible] = useState(true)
   
+  const handleScroll = () => {
+      const currentScrollPos = window.scrollY
+
+      if(currentScrollPos > 80){
+          setVisible(false)
+      }else{
+          setVisible(true)
+      }
+
+  }
+
+  useEffect( () => {
+      window.addEventListener('scroll', handleScroll);
+
+      return () => window.removeEventListener('scroll', handleScroll)
+  })
+
 
   return (  
     <>
-      <NavigationMenu.Root className="pl-5 h-12 md:h-[60px] items-center flex border-b border-zinc-400">
+      {/* improve usability  */}
+      <NavigationMenu.Root className={`bg-white overflow-hidden shadow-md sticky pl-5 h-12 md:h-[60px] items-center border-b transition-all duration-300 border-zinc-400  z-[99999] flex top-0 ${visible ? 'opacity-100 ' : 'opacity-0'}`}> 
         <Breadcrumbs />
       </NavigationMenu.Root>
     </>
